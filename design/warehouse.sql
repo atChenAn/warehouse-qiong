7cd6c11f5a213bcc7f00bc2392273e0bc4e55f93
和风柒月 <971616154@qq.com>
2019-11-07 21:02:10 +0800

    设计文档

--- design/warehouse.sql
+++ design/warehouse.sql
@@ -1,149 +1,147 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2019/11/7 10:37:07                           */
-/*==============================================================*/
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 80012
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : warehouse
 
-drop table if exists bom_code;
+ Target Server Type    : MySQL
+ Target Server Version : 80012
+ File Encoding         : 65001
 
-drop table if exists log;
+ Date: 07/11/2019 21:00:06
+*/
 
-drop table if exists user;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-drop table if exists user_auth;
+-- ----------------------------
+-- Table structure for bom_code
+-- ----------------------------
+DROP TABLE IF EXISTS `bom_code`;
+CREATE TABLE `bom_code`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `bom_code` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `bom_name` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL COMMENT '1、正常\r\n            2、限制\r\n            3、冻结',
+  `is_del` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `AK_Key_2`(`bom_code`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '物料编码表' ROW_FORMAT = Dynamic;
 
-drop table if exists user_group;
+-- ----------------------------
+-- Table structure for log
+-- ----------------------------
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `type` int(11) NULL DEFAULT NULL COMMENT '1、添加\r\n            2、修改\r\n            3、查询\r\n            4、删除\r\n            5、导入\r\n            6、其他',
+  `user_Id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `createTime` int(11) NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL COMMENT '1、成功\r\n            2、失败',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '日志记录' ROW_FORMAT = Dynamic;
 
-drop table if exists warehouse;
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `user_auth` int(11) NULL DEFAULT NULL COMMENT '用户权限，多个权限之间使用逗号分隔\r\n            \r\n            栗子：1,2,4',
+  `nick_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` bigint(20) NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL COMMENT '用户状态：\r\n            0、正常\r\n            1、受限\r\n            2、冻结',
+  `is_del` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `AK_Key_2`(`username`) USING BTREE,
+  UNIQUE INDEX `username_uniq`(`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表，不包含其他附属信息' ROW_FORMAT = Dynamic;
 
-drop table if exists warehouse_detail;
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('642088159705825280', 'admin', '6b1aa0bab50a623b5cde34224eb67573', NULL, '和风七月', 1573127346696, 0, 0);
 
-/*==============================================================*/
-/* Table: bom_code                                              */
-/*==============================================================*/
-create table bom_code
-(
-   id                   bigint not null,
-   bom_code             varchar(100),
-   bom_name             varchar(300),
-   status               int(11) comment '1、正常
-            2、限制
-            3、冻结',
-   is_del               tinyint,
-   primary key (id)
-);
+-- ----------------------------
+-- Table structure for user_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `user_auth`;
+CREATE TABLE `user_auth`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `userId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `groupId` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户组关联表' ROW_FORMAT = Dynamic;
 
-alter table bom_code comment '物料编码表';
+-- ----------------------------
+-- Records of user_auth
+-- ----------------------------
+INSERT INTO `user_auth` VALUES ('642088159726796800', '642088159705825280', '1');
+INSERT INTO `user_auth` VALUES ('642088159726796801', '642088159705825280', '2');
 
-/*==============================================================*/
-/* Table: log                                                   */
-/*==============================================================*/
-create table log
-(
-   id                   varchar(50) not null,
-   type                 int comment '1、添加
-            2、修改
-            3、查询
-            4、删除
-            5、导入
-            6、其他',
-   user_Id              varchar(50),
-   createTime           int,
-   status               int comment '1、成功
-            2、失败',
-   description          text,
-   primary key (id)
-);
+-- ----------------------------
+-- Table structure for user_group
+-- ----------------------------
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE `user_group`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `group_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `group_auths` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '多个权限值外键id，之间使用逗号分隔\r\n            \r\n            栗子：1,2,4\r\n            \r\n            保留字段：目前暂不设计分权管理',
+  `group_status` int(11) NULL DEFAULT NULL COMMENT '组状态：\r\n            0：正常\r\n            1：受限\r\n            2：冻结',
+  `remark` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `is_del` tinyint(4) NULL DEFAULT NULL COMMENT '0 正常\r\n            1 删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户分组表不包含其他附属信息' ROW_FORMAT = Dynamic;
 
-alter table log comment '日志记录';
+-- ----------------------------
+-- Records of user_group
+-- ----------------------------
+INSERT INTO `user_group` VALUES ('1', '系统管理员', NULL, 0, '系统管理员', 0);
+INSERT INTO `user_group` VALUES ('2', 'PM', NULL, 0, '生产计划管理人员', 0);
+INSERT INTO `user_group` VALUES ('3', '仓储管理员', NULL, 0, '仓库管理人员', 0);
+INSERT INTO `user_group` VALUES ('4', '其他人员', NULL, 0, '其他具备数据查看权限的人员', 0);
 
-/*==============================================================*/
-/* Table: user                                                  */
-/*==============================================================*/
-create table user
-(
-   id                   bigint not null,
-   username             varchar(50),
-   password             varchar(50),
-   user_auth            int(11) comment '用户权限，多个权限之间使用逗号分隔
-            
-            栗子：1,2,4',
-   nick_name            varchar(50),
-   create_time          bigint,
-   status               int(11) comment '用户状态：
-            0、正常
-            1、受限
-            2、冻结',
-   is_del               tinyint,
-   primary key (id)
-);
+-- ----------------------------
+-- Table structure for warehouse
+-- ----------------------------
+DROP TABLE IF EXISTS `warehouse`;
+CREATE TABLE `warehouse`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `bom_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `planner_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `repertory` float(11, 4) NULL DEFAULT NULL,
+  `plan_demand` float(11, 4) NULL DEFAULT NULL,
+  `create_time` int(11) NULL DEFAULT NULL,
+  `is_del` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-alter table user comment '用户表，不包含其他附属信息';
+-- ----------------------------
+-- Table structure for warehouse_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `warehouse_detail`;
+CREATE TABLE `warehouse_detail`  (
+  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `warehourse_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `date` int(11) NULL DEFAULT NULL,
+  `plan_count` int(11) NULL DEFAULT NULL,
+  `remark` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `creat_time` int(11) NULL DEFAULT NULL,
+  `is_del` tinyint(4) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '盘存明细表' ROW_FORMAT = Dynamic;
 
-/*==============================================================*/
-/* Table: user_auth                                             */
-/*==============================================================*/
-create table user_auth
-(
-   id                   varchar(50) not null,
-   userId               varchar(50),
-   groupId              varchar(50),
-   primary key (id)
-);
+-- ----------------------------
+-- View structure for v_user
+-- ----------------------------
+DROP VIEW IF EXISTS `v_user`;
+CREATE ALGORITHM = UNDEFINED DEFINER = `root`@`localhost` SQL SECURITY DEFINER VIEW `v_user` AS select `user`.`id` AS `id`,`user`.`username` AS `username`,`user`.`password` AS `password`,`user`.`user_auth` AS `user_auth`,`user`.`nick_name` AS `nick_name`,`user`.`create_time` AS `create_time`,`user`.`status` AS `status`,`user`.`is_del` AS `is_del`,`user_auth`.`groupId` AS `groupId`,`user_group`.`group_name` AS `group_name` from ((`user` left join `user_auth` on((`user`.`id` = `user_auth`.`userId`))) left join `user_group` on((`user_auth`.`groupId` = `user_group`.`id`)));
 
-alter table user_auth comment '用户组关联表';
-
-/*==============================================================*/
-/* Table: user_group                                            */
-/*==============================================================*/
-create table user_group
-(
-   id                   int(11) not null,
-   group_name           varchar(50),
-   group_auths          varchar(200) comment '多个权限值外键id，之间使用逗号分隔
-            
-            栗子：1,2,4',
-   group_status         int(11) comment '组状态：
-            0：正常
-            1：受限
-            2：冻结',
-   remark               varchar(200),
-   is_del               tinyint,
-   primary key (id)
-);
-
-alter table user_group comment '用户分组表不包含其他附属信息';
-
-/*==============================================================*/
-/* Table: warehouse                                             */
-/*==============================================================*/
-create table warehouse
-(
-   id                   int(11) not null,
-   bom_id               int(11),
-   planner_id           int(11),
-   repertory            float(11,4),
-   plan_demand          float(11,4),
-   create_time          int(11),
-   is_del               tinyint,
-   primary key (id)
-);
-
-/*==============================================================*/
-/* Table: warehouse_detail                                      */
-/*==============================================================*/
-create table warehouse_detail
-(
-   id                   int(11) not null,
-   warehourse_id        int(11),
-   name                 varchar(100),
-   date                 int(11),
-   plan_count           int(11),
-   remark               varchar(200),
-   creat_time           int(11),
-   is_del               tinyint,
-   primary key (id)
-);
-
-alter table warehouse_detail comment '盘存明细表';
-
+SET FOREIGN_KEY_CHECKS = 1;
